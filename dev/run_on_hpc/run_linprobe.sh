a856e890e124cf284713ba0c23bdeed745be282e
Kutay Eroğlu <kutay_eroglu@outlook.com>
2025-10-30 11:22:17 +0300

    chore: Update run_linprobe.sh to redirect logs to a specific directory
    - Changed output and error log paths to `ijepa/logs/` for better organization.
    - Added a command to ensure the logs directory exists before job execution.

--- dev/run_on_hpc/run_linprobe.sh
+++ dev/run_on_hpc/run_linprobe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=eval_lin_probe 
-#SBATCH --output=eval_lin_probe-%j.out
-#SBATCH --error=eval_lin_probe-%j.err
+#SBATCH --output=ijepa/logs/eval_lin_probe-%j.out
+#SBATCH --error=ijepa/logs/eval_lin_probe-%j.err
 
 #SBATCH --container-image ghcr.io\#kutayeroglu/ijepa
 #SBATCH --container-mounts /stratch/dataset:/datasets
@@ -12,6 +12,9 @@
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
+
+# Ensure logs directory exists
+mkdir -p ijepa/logs
 
 echo "--- Starting Job: $SLURM_JOB_ID ---"
 echo "Running on host: $(hostname)"
