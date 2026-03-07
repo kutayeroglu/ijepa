29ca4debfdc7ee5921918f22099567fb214e080b
Kutay Eroğlu <kutay_eroglu@outlook.com>
2026-03-07 20:08:47 +0300

    chore: training config changes

--- dev/run_on_hpc/mn5/train_multinoise.sh
+++ dev/run_on_hpc/mn5/train_multinoise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=aw_mnoise
-#SBATCH --qos=acc_ehpc
+#SBATCH --qos=acc_debug
 #SBATCH --account=etur91 
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
@@ -52,7 +52,7 @@ BIND_ARGS="$LOCAL_DATA_DIR:/mnt/data/imagenet,$REAL_LOG_PATH:/mnt/logs,$GREEN_NO
 SIF_IMAGE="/gpfs/projects/etur91/boga222803/ijepa-env.sif"
 
 CMD_ARGS=(
-    --fname configs/lufer_mnoise.yaml
+    --fname configs/balon_mnoise.yaml
     --devices cuda:0 cuda:1 cuda:2 cuda:3
 )
 
